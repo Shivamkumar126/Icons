9bf703fd7448e3390dd9b737921257f3ed67fa46
Shivam <dmpshivamkumar@gmail.com>
2023-10-04 15:33:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,92 +3,92 @@
     {
         id: 1,
         name:"Anomaly-Detector",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Anomaly-Detector.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Anomaly-Detector.svg"
     },
     {
         id: 2,
         name:"Azure-Applied-AI-Services",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Azure-Applied-AI-Services.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Azure-Applied-AI-Services.svg"
     },
     {
         id: 3,
         name:"Azure-Experimentation-Studio",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Azure-Experimentation-Studio.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Azure-Experimentation-Studio.svg"
     },
     {
         id: 4,
         name:"Object-Understanding",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Azure-Object-Understanding.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Azure-Object-Understanding.svg"
     },
     {
         id: 5,
         name:"Bonsai",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Bonsai.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Bonsai.svg"
     },
     {
         id: 6,
         name:"Bot-Services",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Bot-Services.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Bot-Services.svg"
     },
     {
         id: 7,
         name:"Cognitive-Search",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Cognitive-Search.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Cognitive-Search.svg"
     },
     {
         id: 8,
         name:"Computer-Vision",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Computer-Vision.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Computer-Vision.svg"
     },
     {
         id: 9,
         name:"Content-Moderators",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Content-Moderators.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Content-Moderators.svg"
     },
     {
         id: 10,
         name:"Custom-Vision",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Custom-Vision.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Custom-Vision.svg"
     },
     {
         id: 11,
         name:"Face-APIs",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Face-APIs.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Face-APIs.svg"
     },
     {
         id: 13,
         name:"Form-Recognizer",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Form-Recognizers.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Form-Recognizers.svg"
     },
     {
         id: 15,
         name:"Genomics",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Genomics.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Genomics.svg"
     },
 
     {
         id: 16,
         name:"Immersive-Readers",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Immersive-Readers.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Immersive-Readers.svg"
     },
     {
         id: 17,
         name:"Language-Understanding",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Language-Understanding.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Language-Understanding.svg"
     },
     {
         id: 18,
         name:"Personalizers",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Personalizers.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Personalizers.svg"
     },{
         id: 19,
         name:"QnA-Makers",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/QnA-Makers.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/QnA-Makers.svg"
     },
     {
         id: 20,
         name:"Translator-Text",
-        src: "/Azure_Public_Service_Icons/ai_machinelearning/Translator-Text.svg"
+        src: "./Azure_Public_Service_Icons/ai_machinelearning/Translator-Text.svg"
     }
 ]
 
